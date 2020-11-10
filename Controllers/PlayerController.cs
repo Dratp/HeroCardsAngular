91dcf47daceec5dcf1434c5bb2a1d6ee0af3ffdf
David Heizer <DavidHeizer@QuickenLoans.com>
2020-11-10 13:37:38 -0500

    Databse schema and partial data uploaded,  also first API call completed

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AngularHeroCards.Models;
+using AngularHeroCards.Services;
+
+namespace AngularHeroCards.Controllers
+{
+    [Route("api/Player")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        IGameData gameData;
+
+        public PlayerController(IGameData _db)
+        {
+            gameData = _db;
+        }
+
+        [HttpGet]
+        [Route("{playerID}")]
+        public Player GetPlayer (long playerID)
+        {
+            Player currentPlayer = Player.AssemblePlayer(playerID, gameData);
+            return currentPlayer;
+
+        }
+
+
+
+    }
+}
